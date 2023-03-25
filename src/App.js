f8df7e7d3da28eadc1052a17c2622f8dc46735c2
misternooblet <dragonroadsupport@gmail.com>
2023-03-26 00:31:56 +0300

    routes and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { Account, Accounts, Error404, Home, Layout, Transaction, Transactions, User, Users } from "./pages";
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <Error404 />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/accounts', element: <Accounts /> },
+        { path: '/accounts/:id', element: <Account /> },
+        { path: '/users', element: <Users /> },
+        { path: '/users/:id', element: <User /> },
+        { path: '/transactions', element: <Transactions /> },
+        { path: '/transactions/:id', element: <Transaction /> },
+      ]
+
+    },
+
+  ])
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Account from "./Account/Account.page";
+import Accounts from "./Accounts/Accounts.page";
+import User from "./User/User.page";
+import Users from "./Users/Users.page";
+import Transaction from "./Transaction/Transaction.page";
+import Transactions from "./Transactions/Transactions.page";
+import Error404 from "./Error404/Error404.page";
+import Layout from "./Layout/Layout.page";
+import Home from "./Home/Home.page";
+
+export { Account, Accounts, User, Users, Transaction, Transactions, Error404, Layout, Home }
